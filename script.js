fd1c86a0f30e91b2ac35e103a80127c29f60a2ad
Aman Trivedi <amantrivedi598@gmail.com>
2022-10-27 07:47:55 +0530

    final Commit

--- script.js
+++ script.js
@@ -2,15 +2,13 @@ const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
+const Text = document.getElementById('none');
 
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
     mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
-
-
 
 function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
@@ -22,9 +20,9 @@ function getMealList(){
             data.meals.forEach(meal => {
                 html += `
                     <div class = "meal-item" data-id = "${meal.idMeal}">
-                        <div class = "meal-img">
-                            <img src = "${meal.strMealThumb}" alt = "food">
-                        </div>
+                    <div class="meal-img ">
+                    <img class="imgg" src=" ${meal.strMealThumb}" />
+                  </div>
                         <div class = "meal-name">
                             <h3>${meal.strMeal}</h3>
                             <a href = "#" class = "recipe-btn">Get Recipe</a>
@@ -36,13 +34,12 @@ function getMealList(){
         } else{
             html = "Sorry, we didn't find any meal!";
             mealList.classList.add('notFound');
+           
         }
-
         mealList.innerHTML = html;
     });
+   
 }
-
-
 
 function getMealRecipe(e){
     e.preventDefault();
@@ -53,7 +50,6 @@ function getMealRecipe(e){
         .then(data => mealRecipeModal(data.meals));
     }
 }
-
 
 function mealRecipeModal(meal){
     console.log(meal);
@@ -75,3 +71,4 @@ function mealRecipeModal(meal){
     mealDetailsContent.innerHTML = html;
     mealDetailsContent.parentElement.classList.add('showRecipe');
 }
+
